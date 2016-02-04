9c2fdf71d72ebd496047ea6e798c9c7d474cc036
samrg472 <samrg472@gmail.com>
2016-02-04 12:43:23 -0800

    Add a tool to generate a database filled with random data

---
+++ tool/README.md
@@ -0,0 +1,4 @@
+# tool
+
+A simple tool used to generate a database filled with random data
+for testing.
---
+++ tool/src/main/java/org/dsa/iot/etsdb/tool/Main.java
@@ -0,0 +1,41 @@
+package org.dsa.iot.etsdb.tool;
+
+import org.dsa.iot.dslink.node.value.Value;
+import org.dsa.iot.etsdb.serializer.ByteData;
+import org.dsa.iot.etsdb.serializer.ValueSerializer;
+import org.etsdb.Database;
+import org.etsdb.DatabaseFactory;
+import org.etsdb.Serializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.Random;
+
+/**
+ * @author Samuel Grenier
+ */
+public class Main {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+    private static final Random RANDOM = new Random();
+
+    public static void main(String[] args) throws Exception {
+        File file = new File("test_db");
+        Serializer<ByteData> ser = new ValueSerializer();
+        Database<ByteData> db = DatabaseFactory.createDatabase(file, ser);
+
+        ByteData bd = new ByteData();
+        bd.setValue(new Value(RANDOM.nextInt()));
+        for (int i = 0; i < 10000000;) {
+            db.write("test", System.currentTimeMillis(), bd);
+            bd.getValue().set(RANDOM.nextInt());
+            if (++i % 100000 == 0) {
+                LOGGER.info("Wrote {} rows", i);
+            }
+        }
+        db.close();
+        LOGGER.info("Completed writing");
+    }
+
+}
